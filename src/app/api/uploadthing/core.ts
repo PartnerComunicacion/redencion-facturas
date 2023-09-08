1436c08476ea5bb7bd91a118e75c1ff68b098a79
Web <web@Felipe-Pedreros.local>
2023-09-08 17:02:41 -0500

    commit

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,17 +1,18 @@
+// import { currentUser } from '@clerk/nextjs';
 import { createUploadthing, type FileRouter } from 'uploadthing/next';
+import { getServerSession } from 'next-auth';
 
 const f = createUploadthing();
-
-const auth = (req: Request) => ({ id: 'fakeId' }); // Fake auth function
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
 	// Define as many FileRoutes as you like, each with a unique routeSlug
-	imageUploader: f({ image: { maxFileSize: '4MB' } })
+	productImage: f({ image: { maxFileSize: '1MB', maxFileCount: 1 } })
 		// Set permissions and file types for this FileRoute
-		.middleware(async ({ req }) => {
+		.middleware(async (req) => {
 			// This code runs on your server before upload
-			const user = await auth(req);
+			const { user } = useAuth();
+			console.log(user);
 
 			// If you throw, the user will not be able to upload
 			if (!user) throw new Error('Unauthorized');
@@ -19,6 +20,7 @@ export const ourFileRouter = {
 			// Whatever is returned here is accessible in onUploadComplete as `metadata`
 			return { userId: user.id };
 		})
+		// eslint-disable-next-line @typescript-eslint/require-await
 		.onUploadComplete(async ({ metadata, file }) => {
 			// This code RUNS ON YOUR SERVER after upload
 			console.log('Upload complete for userId:', metadata.userId);
